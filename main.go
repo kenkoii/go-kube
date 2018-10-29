126da952c929bbcd5845a9f97cc5009392f60ca3
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-10-29 09:25:40 +0800

    Updated Yaml file and used logger middleware from echo

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
 )
 
 func main() {
 	e := echo.New()
 
+	e.Use(mw.Logger())
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
