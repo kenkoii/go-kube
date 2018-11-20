06806eaeecc340941be89c8d61f04bd8fd3cac1b
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-11-20 19:22:58 +0800

    Reverted to original

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 
 	e.Use(mw.Logger())
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World v2")
+		return c.String(http.StatusOK, "Hello World")
 	})
 
 	e.Logger.Fatal(e.Start(":3000"))
